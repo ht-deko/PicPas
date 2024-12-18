b52e7df753395cf8084ec1cb30d9500e8a7f4d14
DEKO <deko@ht-deko.minim.ne.jp>
2024-12-18 22:02:38 +0900

    Add Japanese Language

---
+++ language/tra_CfgSyntax.pas
@@ -0,0 +1,10 @@
+//Editor Syntax Color settings
+Label3.Caption        := trans('Language:', '' ,'', '', '', '', '', '言語:');
+chkAutoComp.Caption   := trans('Auto Completion', '' ,'', '', '', '', '', '自動補完');
+Label1.Caption        := trans('Token type:', '' ,'', '', '', '', '', '要素:');
+Label2.Caption        := trans('Text Color:', '' ,'', '', '', '', '', 'テキスト色:');
+Label4.Caption        := trans('Back Color:', '' ,'', '', '', '', '', '背景色:');
+chkBold.Caption       := trans('Bold', '' ,'', '', '', '', '', '太字');
+chkItalic.Caption     := trans('Italic', '' ,'', '', '', '', '', '斜体');
+chkUnder.Caption      := trans('Underline', '' ,'', '', '', '', '', '下線');
+
--- language/tra_FrameSyntaxTree.pas
+++ language/tra_FrameSyntaxTree.pas
@@ -1,17 +1,17 @@
 TIT_MAIN := trans('Program'    ,'Programa'   , ''     ,
-                  'Programm'   , 'Програма'  , 'Программа', 'Programme');
+                  'Programm'   , 'Програма'  , 'Программа', 'Programme', 'プログラム');
 TIT_UNIT := trans('Units'      ,'Unidades'   , ''     ,
-                  'Units'      , 'Модулі'    , 'Модули', 'Unités');
+                  'Units'      , 'Модулі'    , 'Модули', 'Unités', 'ユニット');
 TIT_CONS := trans('Constants'  ,'Constantes' , ''     ,
-                  'Konstanten' , 'Константи' , 'Константы', 'Constantes');
+                  'Konstanten' , 'Константи' , 'Константы', 'Constantes', '定数');
 TIT_VARS := trans('Variables'  ,'Variables'  , ''     ,
-                  'Variablen'  , 'Змінні'    , 'Переменные', 'Variables');
+                  'Variablen'  , 'Змінні'    , 'Переменные', 'Variables', '変数');
 TIT_FUNC := trans('Functions'  ,'Funciones'  , ''     ,
-                  'Funktionen' , 'Функції'   , 'Функции', 'Fonctions');
+                  'Funktionen' , 'Функції'   , 'Функции', 'Fonctions', '関数');
 TIT_TYPE := trans('Types'      ,'Tipos'      , ''     ,
-                  ''           , 'Типи'      , 'Типы', 'Types');
+                  ''           , 'Типи'      , 'Типы', 'Types', '型');
 TIT_OTHER:= trans('Others'     ,'Otros'      , ''     ,
-                  'Anderes'    , 'Інше'      , 'Другое', 'Autres');
+                  'Anderes'    , 'Інше'      , 'Другое', 'Autres', 'その他');
 Label1.Caption := Trans('Code Explorer', 'Explorador de Código','',
-                        'Quellenexplorer','Оглядач коду','Инспектор кода', 'Explorateur de Code');
+                        'Quellenexplorer','Оглядач коду','Инспектор кода', 'Explorateur de Code', 'コードエクスプローラ');
 
